9a3bbb7aef65ad395e17856f962290fec98639aa
Zach McCown <zmccown@rollins.edu>
2025-12-02 10:36:47 -0500

    feat(complete): pop-up window success or crash showing stats

--- Heli Hero/Assets/Scripts/LandedUI.cs
+++ Heli Hero/Assets/Scripts/LandedUI.cs
@@ -6,5 +6,36 @@ public class LandedUI : MonoBehaviour {
     [SerializeField] private TextMeshProUGUI titleTextMesh;
     [SerializeField] private TextMeshProUGUI statsTextMesh;
 
+
+    private void Start() {
+        Lander.Instance.OnLanded += Lander_OnLanded;
+
+        Hide();
+    }
+
+    private void Lander_OnLanded(object sender, Lander.OnLandedEventArgs e) {
+        if (e.landingType == Lander.LandingType.Success) {
+            titleTextMesh.text = "SUCCESSFUL LANDING!";
+        } else {
+            titleTextMesh.text = "<color=#ff0000>CRASH!</color>";
+        }
+
+        statsTextMesh.text =
+            Mathf.Round(e.landingSpeed * 2f) + "\n" +
+            Mathf.Round(e.dotVector * 100f) + "%\n" +
+            "x" + e.scoreMultiplier + "\n" +
+            e.score;
+
+        Show();
+    }
+
+    private void Show() {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide() {
+        gameObject.SetActive(false);
+    }
+
 }
 
--- Heli Hero/Assets/Scripts/Lander.cs
+++ Heli Hero/Assets/Scripts/Lander.cs
@@ -15,7 +15,18 @@ public class Lander : MonoBehaviour {
     public event EventHandler <OnLandedEventArgs> OnLanded;
 
     public class OnLandedEventArgs : EventArgs {
+        public LandingType landingType;
         public int score;
+        public float dotVector;
+        public float landingSpeed;
+        public float scoreMultiplier;
+    }
+
+    public enum LandingType {
+        Success,
+        WrongLandingArea,
+        TooSteepAngle,
+        TooFastLanding,
     }
 
     private Rigidbody2D landerRigidbody2D;
@@ -57,20 +68,41 @@ public class Lander : MonoBehaviour {
     private void OnCollisionEnter2D(Collision2D collision2D) {
         if (!collision2D.gameObject.TryGetComponent(out LandingPad landingPad)) {
             Debug.Log("Crashed on terrain");
+            OnLanded?.Invoke(this, new OnLandedEventArgs{
+                landingType = LandingType.WrongLandingArea,
+                dotVector = 0f,
+                landingSpeed = 0f,
+                scoreMultiplier = 0,
+                score = 0,
+            });
             return;
         }
 
         float dotVector = Vector2.Dot(Vector2.up, transform.up);
+        float relativeVelocityMagnitude = collision2D.relativeVelocity.magnitude;
         float minDotVector = .9f;
         if (dotVector < minDotVector) {
             Debug.Log("Landed on a bad angle");
+            OnLanded?.Invoke(this, new OnLandedEventArgs{
+                landingType = LandingType.TooSteepAngle,
+                dotVector = dotVector,
+                landingSpeed = relativeVelocityMagnitude,
+                scoreMultiplier = 0,
+                score = 0,
+            });
             return;
         }
 
-        float relativeVelocityMagnitude = collision2D.relativeVelocity.magnitude;
         float softLandingVecloityMagnitude = 4f;
         if (relativeVelocityMagnitude > softLandingVecloityMagnitude) {
             Debug.Log("Landed too fast");
+            OnLanded?.Invoke(this, new OnLandedEventArgs{
+                landingType = LandingType.TooFastLanding,
+                dotVector = 0f,
+                landingSpeed = relativeVelocityMagnitude,
+                scoreMultiplier = 0,
+                score = 0,
+            });
             return;
         }
 
@@ -94,6 +126,10 @@ public class Lander : MonoBehaviour {
 
         Debug.Log("Score: " + score);
         OnLanded?.Invoke(this, new OnLandedEventArgs{
+            landingType = LandingType.Success,
+            dotVector = dotVector,
+            landingSpeed = relativeVelocityMagnitude,
+            scoreMultiplier = landingPad.GetScoreMultiplier(),
             score = score
         });
     }
