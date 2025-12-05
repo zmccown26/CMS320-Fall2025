099afa4da38c6f2f8552a1605adbf1305a793551
WestonH5 <whollmann@rollins.edu>
2025-12-05 15:59:10 -0500

    Added quitButton

--- Heli Hero/Assets/Scripts/PausedUI.cs
+++ Heli Hero/Assets/Scripts/PausedUI.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PausedUI : MonoBehaviour
 {
     [SerializeField] private Button resumeButton;
+    [SerializeField] private Button quitButton;
 
     private void Awake() {
         resumeButton.onClick.AddListener(() => {
             GameManager.Instance.ResumeGame();
         });
+        
+        quitButton.onClick.AddListener(() => {
+            // Resume game first to restore timeScale
+            GameManager.Instance.ResumeGame();
+            // Then load main menu by scene index (0 = Main_menu)
+            SceneManager.LoadScene(0);
+        });
     }
-
     private void Start() {
         GameManager.Instance.OnGamePaused += GameManager_OnGamePaused;
         GameManager.Instance.OnGameResumed += GameManager_OnGameResumed;
